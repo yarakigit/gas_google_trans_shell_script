40ef9bce44a173dc36b8070348201b9276d66749
yarakigit <ya.tech.go@gmail.com>
2022-11-25 19:14:28 +0900

    update trans_jp.sh

--- trans_jp.sh
+++ trans_jp.sh
@@ -3,4 +3,5 @@ GAS_URL=`echo ${GAS_URL} | sed -e 's/\"//g'`
 
 JP_TXT=$1
 JP_TXT=`echo "$JP_TXT" | nkf -WMQ | tr = %`
+JP_TXT=`echo $JP_TXT | sed -e "s; ;%20;g"`
 curl -L "$GAS_URL?trans=$JP_TXT&jp2eg=1&eg2jp=0" | sed -e 's/\[//g' | sed -e 's/\]//g' | jq '.string'
